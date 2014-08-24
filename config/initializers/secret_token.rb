f67fa2b3b913bab37ccae7b4794865138b7b8bd7
Scott Fowler <shokwave.sf@gmail.com>
2014-08-25 02:36:07 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HearthstoneApp::Application.config.secret_key_base = 'fcdfa179a838757651b2337c6bd4fc0167bf7c0da51d4529fb7153a94cd305908871c53de359614cfa313eec0c1a7bbd8338617fd0269dee7f2f04e93581aaf3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HearthstoneApp::Application.config.session_store :cookie_store, key: '_hearthstone_app_session'
