d011953e7b102510215650d3d993b2474a0e4e77
Scott Fowler <shokwave.sf@gmail.com>
2014-08-25 03:37:46 +1000

    getting into ERb now

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe 'Static pages' do
 			visit '/static_pages/home'
 			expect(page).to have_content 'Sample App'
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/home'
+			expect(page).to have_title "Ruby on Rails Hearthstone App | Home"
+		end
 	end
 
 	describe 'Help page' do
@@ -14,12 +19,22 @@ describe 'Static pages' do
 			visit '/static_pages/help'
 			expect(page).to have_content 'Help'
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/help'
+			expect(page).to have_title "Help"
+		end
 	end
 
 	describe 'About page' do
 		it 'should have the content "About Me"' do
 			visit '/static_pages/about'
 			expect(page).to have_content 'About Me'
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+			expect(page).to have_title 'About'
 		end
 	end
 end
