8f2e73d187883b154ea3e736cfc1e7defcb4e6c5
Scott Fowler <shokwave.sf@gmail.com>
2014-08-27 04:34:16 +1000

    finish layouts and routes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,8 +7,8 @@ describe "UserPages" do
 	describe "signup page" do
 		before { visit signup_path }
 
-		it { should have_content 'Signup' }
-		it { should have_title(full_title('Signup')) }
+		it { should have_content 'Sign Up' }
+		it { should have_title(full_title('Sign Up')) }
 
 	end
 end
